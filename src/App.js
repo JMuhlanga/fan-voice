88d17d71632ad052488e0ec2ae28b18cad9b2fc9
Jose Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-12-14 09:22:23 +0300

    Installation of React Router Dom and other activities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("https://paw-finder-production.up.railway.app/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, [])
+
+  if (!user) return (
+    <>
+    <Router>
+      <Switch>
+        <Route><Login onLogin={setUser} signup = {<Signup onLogin={setUser}/>} /></Route>
+      </Switch>
+    </Router>
+    </>
+);
   return (
     <div className="App">
       
